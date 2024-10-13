6b0a4ffa8a00dbddaa43da08f767e7e2c3654449
Jia Sui <jsfaint@gmail.com>
2024-10-13 11:09:16 +0800

    Evolution to async/await
    1. Split common function to lib
    2. Rewrite with async/await
    
    Signed-off-by: Jia Sui <jsfaint@gmail.com>

--- background.js
+++ background.js
@@ -1,38 +1,38 @@
-chrome.webNavigation.onBeforeNavigate.addListener(function (details) {
-    if (!details) {
-        return;
-    }
+try {
+    importScripts('lib/blacklist.js');
+} catch (e) {
+    console.error(e);
+}
 
-    // 首先，查询当前活动标签的信息
-    chrome.tabs.get(details.tabId, function (tab) {
+chrome.webNavigation.onBeforeNavigate.addListener(async (details) => {
+    try {
+        const tab = await chrome.tabs.get(details.tabId);
         if (chrome.runtime.lastError) {
             return; // 如果获取标签信息失败，直接返回
         }
 
-        if (!tab || tab.incognito) {
+        if (tab === undefined || tab.incognito) {
             return;
         }
 
-        chrome.storage.sync.get(['blacklist'], function (result) {
-            const blacklist = result.blacklist || [];
-            const url = details.url;
+        const url = details.url;
 
-            for (let blockedUrl of blacklist) {
-                if (url.includes(blockedUrl)) {
-                    // 创建一个新的隐私窗口
-                    chrome.windows.create({
-                        url: url,
-                        incognito: true
-                    }, function () {
-                        // 关闭当前标签页
-                        chrome.tabs.remove(details.tabId);
-                        // 从历史记录中删除该网址
-                        chrome.history.deleteUrl({ url: url });
-                    });
+        const blacklist = await getBlacklist();
 
-                    break; // 找到匹配后退出循环
-                }
-            }
-        });
-    });
+        let found = findInList(url, blacklist);
+        if (found) {
+            // 创建一个新的隐私窗口
+            await chrome.windows.create({
+                url: url,
+                incognito: true
+            });
+
+            // 关闭当前标签页
+            await chrome.tabs.remove(details.tabId);
+            // 从历史记录中删除该网址
+            await chrome.history.deleteUrl({ url: url });
+        }
+    } catch (error) {
+        console.error('auto incognito: ', error);
+    }
 }, { url: [{ urlMatches: '.*' }] });
--- popup.js
+++ popup.js
@@ -1,153 +1,120 @@
-document.addEventListener('DOMContentLoaded', () => {
-    function checkWhitelist(url) {
-        const whitelist = new Set([
-            "about:blank",
-            "chrome://",
-            "edge://",
-        ]);
-
-        for (const u of whitelist) {
-            if (url.includes(u)) {
-                return true;
-            }
+document.addEventListener('DOMContentLoaded', async () => {
+    // 点击添加黑名单
+    document.getElementById('addCurrentTabButton').addEventListener('click', async () => {
+        // 获取当前活动的标签页
+        const tabs = await chrome.tabs.query({ active: true, currentWindow: true });
+        if (tabs.length <= 0) {
+            return;
         }
 
-        return false;
-    }
+        const url = tabs[0].url; // 获取当前标签的 URL
+        const hostname = new URL(url).hostname; // 提取域名
 
-    document.getElementById('addCurrentTabButton').addEventListener('click', () => {
-        // 获取当前活动的标签页
-        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-            if (tabs.length <= 0) {
-                return;
-            }
+        if (checkWhitelist(url)) {
+            return;
+        }
 
-            const url = tabs[0].url; // 获取当前标签的 URL
-            const hostname = new URL(url).hostname; // 提取域名
+        // 提取一级域名
+        const parts = hostname.split('.');
+        const tld = parts.pop(); // 顶级域名
+        const secondLevelDomain = parts.pop(); // 第二级域名
+        const primaryDomain = secondLevelDomain + '.' + tld; // 组合成一级域名
 
-            if (checkWhitelist(url)) {
-                return;
-            }
+        // 将一级域名添加到黑名单
+        const blacklist = await getBlacklist();
 
-            // 提取一级域名
-            const parts = hostname.split('.');
-            const tld = parts.pop(); // 顶级域名
-            const secondLevelDomain = parts.pop(); // 第二级域名
-            const primaryDomain = secondLevelDomain + '.' + tld; // 组合成一级域名
+        if (!findInList(primaryDomain, blacklist)) { // 检查是否已存在
+            blacklist.push(primaryDomain);
+            await setBlacklist(blacklist);
+            displayBlacklist();
 
-            // 将一级域名添加到黑名单
-            chrome.storage.sync.get(['blacklist'], (result) => {
-                const blacklist = result.blacklist || [];
-                if (!blacklist.includes(primaryDomain)) { // 检查是否已存在
-                    blacklist.push(primaryDomain);
-                    chrome.storage.sync.set({ blacklist }, displayBlacklist);
-
-                    chrome.tabs.reload(tabs[0].id)
-                }
-            });
-        });
+            chrome.tabs.reload(tabs[0].id)
+        }
     });
 
     // 现有的添加网址到黑名单的代码...
-    document.getElementById('addButton').addEventListener('click', () => {
+    document.getElementById('addButton').addEventListener('click', async () => {
         const urlInput = document.getElementById('urlInput').value.trim();
         if (!urlInput) {
             return;
         }
 
-        chrome.storage.sync.get(['blacklist'], (result) => {
-            const blacklist = result.blacklist || [];
-            if (!blacklist.includes(urlInput)) {
-                blacklist.push(urlInput);
-                chrome.storage.sync.set({ blacklist }, displayBlacklist);
-            }
-        });
+        const blacklist = await getBlacklist();
+        if (!findInList(urlInput, blacklist)) {
+            blacklist.push(urlInput);
+            await setBlacklist(blacklist);
+            displayBlacklist();
+        }
     });
 
     // 显示黑名单
-    function displayBlacklist() {
-        chrome.storage.sync.get(['blacklist'], (result) => {
-            const blacklist = result.blacklist || [];
-            const blacklistElement = document.getElementById('blacklist');
-            blacklistElement.innerHTML = '';
-            blacklist.forEach(url => {
-                const li = document.createElement('li');
-                li.textContent = url;
-                li.addEventListener('click', function () {
-                    removeFromBlacklist(url);
-                });
-                blacklistElement.appendChild(li);
-            });
-        });
-    }
+    async function displayBlacklist() {
+        const blacklist = await getBlacklist();
+        const blacklistElement = document.getElementById('blacklist');
 
-    function removeFromBlacklist(url) {
-        chrome.storage.sync.get(['blacklist'], (result) => {
-            let blacklist = result.blacklist || [];
-            blacklist = blacklist.filter(item => item !== url);
-            chrome.storage.sync.set({ blacklist }, displayBlacklist);
+        blacklistElement.innerHTML = '';
+        blacklist.forEach(url => {
+            const li = document.createElement('li');
+            li.textContent = url;
+            li.addEventListener('click', function () {
+                removeFromBlacklist(url);
+            });
+            blacklistElement.appendChild(li);
         });
     }
 
     const passwordForm = document.getElementById('password-form');
     const verifyPasswordForm = document.getElementById('verify-password-form');
-    const blacklist = document.getElementById('blacklist-manager');
+    const blacklistDiv = document.getElementById('blacklist-manager');
 
     // 检查是否已经设置了密码
-    chrome.storage.sync.get('password', (data) => {
-        if (data.password) {
-            // 如果已经设置了密码，显示验证密码的表单
-            verifyPasswordForm.removeAttribute('hidden');
-            document.getElementById('verify-password').focus();
-        } else {
-            // 如果没有设置密码，显示设置密码的表单
-            passwordForm.removeAttribute('hidden');
-            document.getElementById('new-password').focus();
-        }
-    });
+    const password = await getPassword();
+    if (password) {
+        // 如果已经设置了密码，显示验证密码的表单
+        verifyPasswordForm.removeAttribute('hidden');
+        document.getElementById('verify-password').focus();
+    } else {
+        // 如果没有设置密码，显示设置密码的表单
+        passwordForm.removeAttribute('hidden');
+        document.getElementById('new-password').focus();
+    }
 
-    function setPassword() {
-        const newPassword = document.getElementById('new-password').value;
-        if (newPassword) {
-            chrome.storage.sync.set({ password: newPassword }, () => {
-                verifyPasswordForm.removeAttribute('hidden', '');
-                passwordForm.setAttribute('hidden', '');
+    const newPassword = document.getElementById('new-password').value;
+    if (newPassword) {
+        await setPassword(newPassword);
+        verifyPasswordForm.removeAttribute('hidden', '');
+        passwordForm.setAttribute('hidden', '');
 
-                alert('设置密码成功');
-            });
-        } else {
-            alert('请输入密码');
-        }
+        alert('设置密码成功');
     }
 
     // 设置密码
-    document.getElementById('set-password').addEventListener('click', () => {
-        setPassword();
+    document.getElementById('set-password').addEventListener('click', async () => {
+        await setPassword();
     });
 
     // 设置密码 监听回车键事件
-    document.getElementById('new-password').addEventListener('keydown', (event) => {
+    document.getElementById('new-password').addEventListener('keydown', async (event) => {
         if (event.key === 'Enter') {
-            setPassword();
+            await setPassword();
         }
     });
 
-    function verifyPassword() {
+    async function verifyPassword() {
         const enteredPassword = document.getElementById('verify-password').value;
 
-        chrome.storage.sync.get('password', (data) => {
-            if (enteredPassword === data.password) {
-                verifyPasswordForm.setAttribute('hidden', '');
-                passwordForm.setAttribute('hidden', '');
+        const password = await getPassword();
+        if (enteredPassword === password) {
+            verifyPasswordForm.setAttribute('hidden', '');
+            passwordForm.setAttribute('hidden', '');
 
-                blacklist.removeAttribute('hidden');
+            blacklistDiv.removeAttribute('hidden');
 
-                // 将光标聚焦在输入框上
-                document.getElementById('urlInput').focus();
-            } else {
-                alert('密码错误');
-            }
-        });
+            // 将光标聚焦在输入框上
+            document.getElementById('urlInput').focus();
+        } else {
+            alert('密码错误');
+        }
     }
 
     // 验证密码
