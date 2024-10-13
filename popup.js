1029808cbce6d29a09d4f89ccfb79c43a939ea4c
Jia Sui <jsfaint@gmail.com>
2024-10-13 13:03:41 +0800

    Refactor popup.js
    Signed-off-by: Jia Sui <jsfaint@gmail.com>

--- popup.js
+++ popup.js
@@ -1,6 +1,57 @@
 document.addEventListener('DOMContentLoaded', async () => {
+    const passwordForm = document.getElementById('password-form');
+    const setPasswordButton = document.getElementById('set-password');
+    const verifyPasswordForm = document.getElementById('verify-password-form');
+    const blacklistDiv = document.getElementById('blacklist-manager');
+
+    const inPrivateMode = document.getElementById('in-private-mode');
+
+    const verifyInput = document.getElementById('verify-password');
+    const verifyButton = document.getElementById('verify-password-btn');
+
+    const newPasswordInput = document.getElementById('new-password');
+
+    const addCurrentTabButton = document.getElementById('addCurrentTabButton');
+    const addButton = document.getElementById('addButton');
+    const urlInput = document.getElementById('urlInput');
+
+
+    // 显示黑名单
+    async function displayBlacklist() {
+        const blacklist = await getBlacklist();
+        const blacklistElement = document.getElementById('blacklist');
+
+        blacklistElement.innerHTML = '';
+        blacklist.forEach(url => {
+            const li = document.createElement('li');
+            li.textContent = url;
+            li.addEventListener('click', () => {
+                removeFromBlacklist(url);
+            });
+            blacklistElement.appendChild(li);
+        });
+    }
+
+    // 验证密码
+    async function verifyPassword() {
+        const enteredPassword = verifyInput.value;
+
+        const password = await getPassword();
+        if (enteredPassword === password) {
+            verifyPasswordForm.setAttribute('hidden', '');
+            passwordForm.setAttribute('hidden', '');
+
+            blacklistDiv.removeAttribute('hidden');
+
+            // 将光标聚焦在输入框上
+            urlInput.focus();
+        } else {
+            alert('密码错误');
+        }
+    }
+
     // 点击添加黑名单
-    document.getElementById('addCurrentTabButton').addEventListener('click', async () => {
+    addCurrentTabButton.addEventListener('click', async () => {
         // 获取当前活动的标签页
         const tabs = await chrome.tabs.query({ active: true, currentWindow: true });
         if (tabs.length <= 0) {
@@ -10,7 +61,7 @@ document.addEventListener('DOMContentLoaded', async () => {
         const url = tabs[0].url; // 获取当前标签的 URL
         const hostname = new URL(url).hostname; // 提取域名
 
-        if (checkWhitelist(url)) {
+        if (findInWhitelist(url)) {
             return;
         }
 
@@ -33,97 +84,53 @@ document.addEventListener('DOMContentLoaded', async () => {
     });
 
     // 现有的添加网址到黑名单的代码...
-    document.getElementById('addButton').addEventListener('click', async () => {
-        const urlInput = document.getElementById('urlInput').value.trim();
-        if (!urlInput) {
+    addButton.addEventListener('click', async () => {
+        url = urlInput.trim();
+        if (!url) {
             return;
         }
 
         const blacklist = await getBlacklist();
-        if (!findInList(urlInput, blacklist)) {
-            blacklist.push(urlInput);
+        if (!findInList(url, blacklist)) {
+            blacklist.push(url);
             await setBlacklist(blacklist);
             displayBlacklist();
         }
     });
-
-    // 显示黑名单
-    async function displayBlacklist() {
-        const blacklist = await getBlacklist();
-        const blacklistElement = document.getElementById('blacklist');
-
-        blacklistElement.innerHTML = '';
-        blacklist.forEach(url => {
-            const li = document.createElement('li');
-            li.textContent = url;
-            li.addEventListener('click', function () {
-                removeFromBlacklist(url);
-            });
-            blacklistElement.appendChild(li);
-        });
-    }
-
-    const passwordForm = document.getElementById('password-form');
-    const verifyPasswordForm = document.getElementById('verify-password-form');
-    const blacklistDiv = document.getElementById('blacklist-manager');
 
     // 检查是否已经设置了密码
     const password = await getPassword();
     if (password) {
         // 如果已经设置了密码，显示验证密码的表单
         verifyPasswordForm.removeAttribute('hidden');
-        document.getElementById('verify-password').focus();
+        verifyInput.focus();
     } else {
         // 如果没有设置密码，显示设置密码的表单
         passwordForm.removeAttribute('hidden');
-        document.getElementById('new-password').focus();
-    }
-
-    const newPassword = document.getElementById('new-password').value;
-    if (newPassword) {
-        await setPassword(newPassword);
-        verifyPasswordForm.removeAttribute('hidden', '');
-        passwordForm.setAttribute('hidden', '');
-
-        alert('设置密码成功');
+        newPasswordInput.focus();
     }
 
     // 设置密码
-    document.getElementById('set-password').addEventListener('click', async () => {
-        await setPassword();
-    });
-
-    // 设置密码 监听回车键事件
-    document.getElementById('new-password').addEventListener('keydown', async (event) => {
-        if (event.key === 'Enter') {
-            await setPassword();
-        }
-    });
-
-    async function verifyPassword() {
-        const enteredPassword = document.getElementById('verify-password').value;
-
-        const password = await getPassword();
-        if (enteredPassword === password) {
-            verifyPasswordForm.setAttribute('hidden', '');
+    setPasswordButton.addEventListener('click', async () => {
+        const newPassword = newPasswordInput.value;
+        if (newPassword) {
+            await setPassword(newPassword);
+            verifyPasswordForm.removeAttribute('hidden', '');
             passwordForm.setAttribute('hidden', '');
 
-            blacklistDiv.removeAttribute('hidden');
-
-            // 将光标聚焦在输入框上
-            document.getElementById('urlInput').focus();
+            alert('设置密码成功');
         } else {
-            alert('密码错误');
+            alert('请输入密码！');
         }
-    }
+    });
 
     // 验证密码
-    document.getElementById('verify-password-btn').addEventListener('click', () => {
+    verifyButton.addEventListener('click', () => {
         verifyPassword();
     });
 
     // 验证密码 监听回车键事件
-    document.getElementById('verify-password').addEventListener('keydown', (event) => {
+    verifyInput.addEventListener('keydown', (event) => {
         if (event.key === 'Enter') {
             verifyPassword();
         }
