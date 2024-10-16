bdd4bc03e948147095aa70c74c16f5e2501db522
Jia Sui <jsfaint@gmail.com>
2024-10-16 17:58:59 +0800

    Fix blacklist issue
    Signed-off-by: Jia Sui <jsfaint@gmail.com>

--- background.js
+++ background.js
@@ -1,13 +1,13 @@
 "use strict";
 
 try {
-    importScripts('lib/blacklist.js', 'lib/whitelist.js', 'lib/private.js');
+    importScripts('lib/blacklist.js', 'lib/private.js');
 } catch (e) {
     console.error(e);
 }
 
 chrome.webNavigation.onBeforeNavigate.addListener(async (details) => {
-    // 如果private mode未打开，则不需要在此listener中处理
+    // if private option was disabled, skip it.
     const privateOption = await getPrivateOption();
     if (!privateOption) {
         return;
@@ -20,43 +20,37 @@ chrome.webNavigation.onBeforeNavigate.addListener(async (details) => {
 
     const url = details.url;
 
-    // 如果地址在白名单中，则跳过
-    if (findInWhitelist(url)) {
-        return;
-    }
-
-    const found = findInBlacklist(url);
+    const found = await findInBlacklist(url);
     if (!found) {
         return;
     }
 
-    // 创建一个新的隐私窗口
-    chrome.windows.create({
+    // Create a new private window
+    await chrome.windows.create({
         url: url,
         incognito: true
     });
 
-    // 关闭当前标签页
-    chrome.tabs.remove(details.tabId);
-    // 从历史记录中删除该网址
-    chrome.history.deleteUrl({ url: url });
+    // Close current tab
+    await chrome.tabs.remove(details.tabId);
+    // Clear url history
+    await chrome.history.deleteUrl({ url: url });
 }, { url: [{ urlMatches: '.*' }] });
 
-// 监听标签页更新事件
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     chrome.tabs.onRemoved.addListener(async (tabId, removeInfo) => {
         const privateOption = await getPrivateOption();
-        // 如果private mode已打开，则不需要在此listener中处理
+        // If the private option was enabled, skip it.
         if (privateOption) {
             return;
         }
 
-        const found = findInBlacklist(tab.url);
+        const found = await findInBlacklist(tab.url);
         if (!found) {
             return;
         }
 
-        // 清除该标签页的访问历史
-        chrome.history.deleteUrl({ url: tab.url });
+        // Clear url history of curr
+        await chrome.history.deleteUrl({ url: tab.url });
     });
 });
